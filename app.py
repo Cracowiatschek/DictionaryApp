ff766f40bf5277a1f13f7b542d744fdfa7627ee6
Cracowiatschek <piotrbaran0123@gmail.com>
2024-12-29 17:24:24 +0100

    Dictionary app 2.0 - done

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 from models import db, Camapign
 
@@ -16,6 +18,7 @@ def index():
 
     navbar_class = "blue darken-2"
 
+
     if session.get("logged_in") is True:
         session["table"] = ""
         navbar_class = "green darken-2"
@@ -27,7 +30,11 @@ def index():
 
     filter_campaign = ""
     all_campaigns = ""
+    editable = ""
+
+
     if "campaign" in content.keys():
+        content["campaign"] = [i for i in content["campaign"] if i.saved is False]
         filter_campaign = content["campaign"]
         all_campaigns = content["campaign"]
         # for i,x in enumerate(filter_campaign):
@@ -46,8 +53,12 @@ def index():
                 filter_campaign = [camp for i, camp in enumerate(filter_campaign) if
                                 filter_campaign[i].rt is False]
 
+    if "editable" in content.keys():
+        editable = content["editable"][session["min_id"]:session["max_id"]]
+        print(editable, content["editable"])
+
     return render_template("index.html", navbar_class=navbar_class, campaigns=filter_campaign,
-                           all_campaigns = all_campaigns)
+                           all_campaigns = all_campaigns, editable = editable)
 
 
 @app.route("/login", methods=["POST"])
@@ -55,15 +66,32 @@ def login():
     username = request.form.get("username")
     password = request.form.get("password")
 
+    no_relation_fields = json.load(open("no_relation_fields.json", 'r', encoding = "utf-8"))
+    lv_1 = json.load(open("relation_lv1_lv2.json", 'r', encoding = "utf-8"))
+    lv_2 = json.load(open("relation_lv1_lv2.json", 'r', encoding = "utf-8"))
+    lv_3 = json.load(open("relation_lv2_lv3.json", 'r', encoding = "utf-8"))
+    lv_4 = json.load(open("relation_lv3_lv4.json", 'r', encoding = "utf-8"))
+
     if username == "admin" and password == "admin":
+        session["person"] = no_relation_fields["person"]
+        session["succType"] = no_relation_fields["succType"]
+        session["lv_1"] = [i for i in lv_1]
+        content["lv_2"] = lv_2
+        content["lv_3"] = lv_3
+        content["lv_4"] = lv_4
         session["logged_in"] = True
         session["loading"] = True
+        session["pick_active"] = True
+        session["data_active"] = True
         session["hasData"] = False
+        session["page_size"] = 5
+
         return redirect(url_for("index"))
 
     session["logged_in"] = False
     session.pop("loading", None)
     session.pop("hasData", None)
+
     return redirect(url_for("index"))
 
 
@@ -73,6 +101,8 @@ def logout():
     session.pop("loading", None)
     session.pop("hasData", None)
     session.pop("data", None)
+    session.pop("render_table", None)
+    content.pop("editable", None)
     return redirect(url_for("index"))
 
 
@@ -88,17 +118,19 @@ def get_data():
     return redirect(url_for("index"))
 
 
-@app.route("/send-data", methods=["POST", "GET"])
-def send_data():
+@app.route("/pick-data", methods=["POST", "GET"])
+def pick_data():
     action = request.args.get('action')
     data = request.args.get('data')
+
+    for i,x in enumerate(content["campaign"]):
+        content["campaign"][i].id = i+1
 
     for req in request.form:
         if req.isdigit():
             content["campaign"][int(req)-1].checked = True
 
     if request.form.get("filter-button"):
-        print()
         if request.form.get("filter"):
             session["searchPlaceholder"] = request.form.get("filter")
 
@@ -115,13 +147,12 @@ def send_data():
                 session.pop("searchBatch")
 
     if request.form.get("clear"):
-        for i in ["searchBatch", "searchRT", "searchPlaceholder"]:
-            if i in session:
-                session.pop(i)
-                print(i)
+        for act_id in ["searchBatch", "searchRT", "searchPlaceholder"]:
+            if act_id in session:
+                session.pop(act_id)
 
-        for i,camp in enumerate(content["campaign"]):
-            content["campaign"][i].checked = False
+        for act_id,camp in enumerate(content["campaign"]):
+            content["campaign"][act_id].checked = False
 
     if action == 'check':
         if content["campaign"][int(data)-1].checked:
@@ -132,11 +163,181 @@ def send_data():
     if action == 'uncheck':
         content["campaign"][int(data)-1].checked = False
 
+    if action == 'show':
+        if session.get("pick_active"):
+            session["pick_active"] = False
+        else:
+            session["pick_active"] = True
 
     if request.form.get("edit"):
         session["render_table"] = True
+        if "editable" in content:
+            campaign_names = [content["editable"][i].name for i, n in enumerate(content["editable"])]
+            campaigns = [val for i, val in enumerate(content["campaign"]) if
+                         content["campaign"][i].name not in campaign_names and content["campaign"][i].checked]
+            content["editable"] += campaigns
+
+        else:
+            content["editable"] = [camp for i,camp in enumerate(content["campaign"]) if
+                                   content["campaign"][i].checked]
+
+        content["editable"] = content["editable"].copy()
+
+        for act_id, camp in enumerate(content["editable"]):
+            content["editable"][act_id].id = act_id
+            if type(content["editable"][act_id].lv2_options) != list:
+                if content["editable"][act_id].lv1 in content["lv_2"]:
+                    content["editable"][act_id].lv2_options = content["lv_2"][content["editable"][act_id].lv1]
+                else:
+                    content["editable"][act_id].lv2_options = [content["editable"][act_id].lv2_options]
+
+            if type(content["editable"][act_id].lv3_options) != list:
+                if content["editable"][act_id].lv2 in content["lv_3"]:
+                    content["editable"][act_id].lv3_options = content["lv_3"][content["editable"][act_id].lv2]
+                else:
+                    content["editable"][act_id].lv3_options = [content["editable"][act_id].lv3_options]
+
+            if type(content["editable"][act_id].lv4_options) != list:
+                content["editable"][act_id].lv4_options = [content["editable"][act_id].lv4_options]
+
+        for act_id, camp in enumerate(content["campaign"]):
+            content["campaign"][act_id].id = act_id+1
+
+        for item in ["searchBatch", "searchRT", "searchPlaceholder"]:
+            if item in session:
+                session.pop(item)
+
+        for i,camp in enumerate(content["campaign"]):
+            content["campaign"][i].checked = False
+
+        session["pick_active"] = False
+        session["data_active"] = True
+        session["page"] = 0
+        session["min_page"] = 0
+        session["min_id"] = 0
+        session["max_id"] = min(len(content["editable"]), 5)
+
+        if len(content["editable"]) % session["page_size"] == 0:
+            session["max_page"] = len(content["editable"]) // session["page_size"]
+        else:
+            session["max_page"] = len(content["editable"]) // session["page_size"] + 1
+
+        session["pages"] = list(range(session["max_page"]))
 
     return redirect(url_for("index"))
+
+@app.route("/send-data", methods=["POST", "GET"])
+def send_data():
+    action = request.args.get('action')
+    data = request.args.get('data')
+
+    if request.form.get("clear"):
+        content.pop("editable", None)
+        session.pop("render_table", None)
+        session["pick_active"] = True
+        return redirect(url_for("index"))
+
+    def update_changes(requests):
+        for req in requests:
+            if req.split('-')[0].isdigit():
+                req_id = int(req.split('-')[0])
+
+                camp_id = [i for i, x in enumerate(content["editable"]) if content["editable"][i].id == req_id][0]
+                if "camp_name" in req:
+                    if len(request.form.get(req)) != "":
+                        content["editable"][camp_id].camp_name = request.form.get(req)
+
+                if "person" in req:
+                    if len(request.form.get(req)) != "":
+                        content["editable"][camp_id].person = request.form.get(req)
+
+                if "lv1" in req:
+                    if len(request.form.get(req)) != "":
+                        content["editable"][camp_id].lv1 = request.form.get(req)
+                        content["editable"][camp_id].lv2_options = content["lv_2"][request.form.get(req)]
+
+                if "lv2" in req:
+                    if len(request.form.get(req)) != "":
+                        content["editable"][camp_id].lv2 = request.form.get(req)
+                        content["editable"][camp_id].lv3_options = content["lv_3"][request.form.get(req)]
+
+                if "lv3" in req:
+                    if len(request.form.get(req)) != "":
+                        content["editable"][camp_id].lv3 = request.form.get(req)
+                        content["editable"][camp_id].lv4_options = content["lv_4"][request.form.get(req)]
+
+                if "lv4" in req:
+                    if len(request.form.get(req)) != "":
+                        content["editable"][camp_id].lv4 = request.form.get(req)
+
+                if "succType" in req:
+                    if len(request.form.get(req)) != "":
+                        content["editable"][camp_id].succType = request.form.get(req)
+
+    update_changes(request.form)
+
+    if request.form.get("save"):
+        update_changes(request.form)
+        correct_state = 0
+        for row in content["editable"]:
+            row.wrong = False
+            if row.offline is False:
+                if (row.camp_name != "" and row.camp_name is not None and row.person in session["person"]
+                        and row.lv1 in session["lv_1"] and row.lv2 in row.lv2_options
+                        and row.lv3 in row.lv3_options and row.lv4 in row.lv4_options
+                        and row.succType in session["succType"]):
+                        pass
+                else:
+                    row.wrong = True
+                    correct_state += 1
+
+        if correct_state == 0:
+            return render_template("save_data.html")
+        else:
+            return redirect(url_for("index"))
+
+
+
+    if action == "offline":
+        camp_id = [i for i, x in enumerate(content["editable"]) if content["editable"][i].id == int(data)][0]
+        if content["editable"][camp_id].offline:
+            content["editable"][camp_id].offline = False
+        else:
+            content["editable"][camp_id].offline = True
+
+    if action == "show":
+        if session.get("data_active"):
+            session["data_active"] = False
+        else:
+            session["data_active"] = True
+
+
+    if action == "page":
+        if data.isdigit():
+            session["page"] = int(data)
+            session["min_id"] = session["page"] * session["page_size"]
+            session["max_id"] = min(session["min_id"] + session["page_size"], len(content["editable"]))
+
+        else:
+            if data == "left" and session["page"] > session["min_page"]:
+                session["page"] -= 1
+            elif data == "right" and session["page"] < session["max_page"]:
+                session["page"] += 1
+
+    return redirect(url_for("index"))
+
+
+@app.route("/save-data")
+def save_data():
+    session["render_table"] = False
+    session["pick_active"] = True
+    for i in content["editable"]:
+        i.wrong = False
+        if i.offline is False:
+            i.saved = True
+    content.pop("editable", None)
+    return redirect(url_for("index"))
+
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -5,18 +5,38 @@ db = SQLAlchemy()
 class Camapign(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(80), unique = True, nullable = False)
+    camp_name = db.Column(db.String(80), unique = True, nullable = True)
     person = db.Column(db.String(30), unique = False, nullable = True)
     lv1 = db.Column(db.String(20), unique = False, nullable = True)
+    lv2_options = db.Column(db.String(256), unique = False, nullable = True, default="Brak")
     lv2 = db.Column(db.String(20), unique = False, nullable = True)
+    lv3_options = db.Column(db.String(256), unique = False, nullable = True, default="Brak")
+    lv3 = db.Column(db.String(20), unique = False, nullable = True)
+    lv4_options = db.Column(db.String(256), unique = False, nullable = True, default="Brak")
+    lv4 = db.Column(db.String(20), unique = False, nullable = True)
+    succType = db.Column(db.String(3), unique = False, nullable = True)
     rt = db.Column(db.Boolean, default = False)
     checked = db.Column(db.Boolean, default = False)
+    offline = db.Column(db.Boolean, default = False)
+    wrong = db.Column(db.Boolean, default = False)
+    saved = db.Column(db.Boolean, default = False)
     def to_dict(self):
         return {
             "id": self.id,
             "name": self.name,
+            "camp_name": self.camp_name,
             "person": self.person,
             "lv1": self.lv1,
+            "lv2_options": self.lv2_options,
             "lv2": self.lv2,
+            "lv3_options": self.lv3_options,
+            "lv3": self.lv3,
+            "lv4_options": self.lv4_options,
+            "lv4": self.lv4,
+            "succType": self.succType,
             "rt": self.rt,
-            "checked": self.checked
+            "checked": self.checked,
+            "offline": self.offline,
+            "wrong": self.wrong,
+            "saved": self.saved
         }
